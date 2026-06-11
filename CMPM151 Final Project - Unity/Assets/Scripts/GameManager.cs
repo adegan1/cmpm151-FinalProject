5572d131761942b8a4d432cdb09cb21fc0886788
adegan1 <adegan@ucsc.edu>
2026-06-10 22:39:00 -0700

    Added coin decrease on death to match sound. Fixed ghost hitboxes

--- CMPM151 Final Project - Unity/Assets/Scripts/GameManager.cs
+++ CMPM151 Final Project - Unity/Assets/Scripts/GameManager.cs
@@ -14,6 +14,12 @@ namespace Platformer
         private PlayerController player;
         public GameObject deathPlayerPrefab;
         public Text coinText;
+        [SerializeField] private int coinCountDelayMs = 500;
+        [SerializeField] private int coinCountStepIntervalMs = 100;
+
+        private bool deathSequenceRunning;
+        private bool coinCountLerping;
+        private Coroutine deathSequenceRoutine;
 
         void Start()
         {
@@ -22,15 +28,57 @@ namespace Platformer
 
         void Update()
         {
-            coinText.text = coinsCounter.ToString();
+            if (!coinCountLerping)
+            {
+                coinText.text = coinsCounter.ToString();
+            }
+
             if(player.deathState == true)
             {
-                playerGameObject.SetActive(false);
-                GameObject deathPlayer = (GameObject)Instantiate(deathPlayerPrefab, playerGameObject.transform.position, playerGameObject.transform.rotation);
-                deathPlayer.transform.localScale = new Vector3(playerGameObject.transform.localScale.x, playerGameObject.transform.localScale.y, playerGameObject.transform.localScale.z);
-                player.deathState = false;
-                Invoke("ReloadLevel", 3);
+                StartDeathSequence();
             }
+        }
+
+        private void StartDeathSequence()
+        {
+            if (deathSequenceRunning)
+            {
+                return;
+            }
+
+            deathSequenceRunning = true;
+            playerGameObject.SetActive(false);
+            GameObject deathPlayer = (GameObject)Instantiate(deathPlayerPrefab, playerGameObject.transform.position, playerGameObject.transform.rotation);
+            deathPlayer.transform.localScale = new Vector3(playerGameObject.transform.localScale.x, playerGameObject.transform.localScale.y, playerGameObject.transform.localScale.z);
+            player.deathState = false;
+
+            if (deathSequenceRoutine != null)
+            {
+                StopCoroutine(deathSequenceRoutine);
+            }
+
+            deathSequenceRoutine = StartCoroutine(DeathSequenceRoutine());
+        }
+
+        private IEnumerator DeathSequenceRoutine()
+        {
+            yield return new WaitForSeconds(coinCountDelayMs / 1000f);
+
+            coinCountLerping = true;
+
+            int displayedCoins = coinsCounter;
+            coinText.text = displayedCoins.ToString();
+            int stepIntervalMs = Mathf.Max(1, coinCountStepIntervalMs);
+
+            while (displayedCoins > 0)
+            {
+                yield return new WaitForSeconds(stepIntervalMs / 1000f);
+                displayedCoins--;
+                coinText.text = displayedCoins.ToString();
+            }
+
+            coinCountLerping = false;
+            ReloadLevel();
         }
 
         private void ReloadLevel()
